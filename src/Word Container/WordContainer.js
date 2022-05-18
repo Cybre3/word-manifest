7ff988f13f922932b97172037f669f06a2fa4d70
Cybre <cybre3@gmail.com>
2022-05-18 15:25:21 -0500

    styled app

--- src/Word Container/WordContainer.js
+++ src/Word Container/WordContainer.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Formik, Form } from "formik";
+import html2pdf from "html2pdf.js";
 import FormikControl from "../Formik/FormikControl";
 
 import "./WordContainer.css";
 function WordContainer(props) {
-  const [wordManifest, setWordManifest] = useState("")
+  const [wordManifest, setWordManifest] = useState("");
   const [contentCount, setContentCount] = useState(0);
 
   const initialValues = {
@@ -16,30 +17,68 @@ function WordContainer(props) {
     let removeNotWordCh = /[^\w]/gm;
     let wordsArr = values.content.split(removeNotWordCh).filter((word) => word !== "");
 
-    setContentCount(wordsArr.length);
+    setContentCount(Number(wordsArr.length));
 
     for (let word of wordsArr) {
       !wordCountObj[word] ? (wordCountObj[word] = 1) : wordCountObj[word]++;
     }
-    
-    setWordManifest(JSON.stringify(wordCountObj));
+
+    document.getElementById("count-result").style.border =
+      Number(wordsArr.length) !== 0 ? "3px inset rgb(92, 167, 211)" : null;
+
+    setWordManifest(wordCountObj);
   }
-  console.log(wordManifest, contentCount)
+
+  function printList() {
+    var divContents = document.getElementById("count-result").innerHTML;
+    var a = window.open("", "", "height=500, width=500");
+    a.document.write(divContents);
+    a.print();
+  }
+
+  function download() {
+    var element = document.getElementById("count-result");
+    html2pdf(element);
+  }
 
   return (
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
-        <Form className="container">
-          <FormikControl className='content-box' control="textarea" label="Enter Content Below" name="content" />
-          <div className="btn-container">
-            <button type="submit">Count</button>
-            <button>Clear</button>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Form className="container">
+        <FormikControl
+          className="content-box"
+          control="textarea"
+          label="Enter Content Below"
+          name="content"
+        />
+        <div className="btn-container">
+          <button type="reset" className="clear-btn">
+            Clear
+          </button>
+          <button type="submit" className="count-btn">
+            Count
+          </button>
+        </div>
+        <div className="print-save-btns">
+          <button className="print-btn" onClick={printList}>
+            Print
+          </button>
+          <button className="save-btn" onClick={download}>
+            Save
+          </button>
+        </div>
+        <div className="count-result" id="count-result">
+          <div className="count-result-title">{`Content Count: ${contentCount}`}</div>
+          <div className="entries-box">
+            {Object.entries(wordManifest).map((word, index) => (
+              <div key={index} className="each-entry">
+                <span className="word">{word[0]}</span>
+                <span className="count">{word[1]}</span>
+              </div>
+            ))}
           </div>
-          <div className="count-result">
-            <span>Content Count: {contentCount}</span>
-            <span>{wordManifest}</span>
-          </div>
-        </Form>
-      </Formik>
+        </div>
+      </Form>
+    </Formik>
   );
 }
 
